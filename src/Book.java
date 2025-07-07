c3f5a4209812e3c58b24dae4ffdf88e78a744614
Nurab-13 <senpaiyt289@gmail.com>
2025-07-07 12:20:27 +0545

    Library System

---
+++ src/Book.java
@@ -0,0 +1,15 @@
+public class Book {
+    //attributes-->book number, nam, quantity, author
+    //TODO create the attributes
+    private int bookID;
+    private String bookName;
+    private String author;
+    private String publisher;
+    //behaviours -> increse quantity , decrease quantity
+    public void increaseQantity(){
+        System.out.println("need to work in increase");
+    }
+    public void decreaseQantity(){
+        System.out.println("need to work in decrease");
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,16 @@
+public class User {
+    //attributes-->Name,Contact
+    //TODO create the attributes
+    private int contactId;
+    private String UserName;
+    //TODO behaviours -> Check if registered , borrow , return
+    public void CheckRegistered(){
+        System.out.println("need to work in registration");
+    }
+    public void BorrowingBook(){
+        System.out.println("need to work in borrowing");
+    }
+    public void ReturningBorrowedBooks(){
+        System.out.println("need to work in returning ");
+    }
+}
